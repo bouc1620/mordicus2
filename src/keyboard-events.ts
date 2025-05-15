6d18fdb50a5d30d2187765b291dc85be1dafedcb
Charles Boudreault <charlesboudreault1@gmail.com>
2025-05-14 20:23:46 -0400

    added keyboard event prevent default, avoids scrolling with arrows in brower app

--- src/keyboard-events.ts
+++ src/keyboard-events.ts
@@ -6,16 +6,20 @@ import {
   map,
   switchMap,
   takeUntil,
+  tap,
 } from 'rxjs';
 import * as Directions from './directions';
 
 export class KeyboardEvents {
-  all$ = fromEvent<KeyboardEvent>(window, 'keydown').pipe(map((event) => event.key));
+  private _keydownEvent$ = fromEvent<KeyboardEvent>(window, 'keydown').pipe(
+    tap((event) => event.preventDefault()),
+  );
+  all$ = this._keydownEvent$.pipe(map((event) => event.key));
   number$ = this.all$.pipe(filter((key) => /^[0-9]$/.test(key)));
   confirm$ = this.all$.pipe(filter((key) => key === 'Enter' || key === ' '));
   escape$ = this.all$.pipe(filter((key) => key === 'Escape'));
   backspace$ = this.all$.pipe(filter((key) => key === 'Backspace'));
-  direction$ = fromEvent<KeyboardEvent>(window, 'keydown').pipe(
+  direction$ = this._keydownEvent$.pipe(
     filter((event) => !event.repeat),
     map((event) => event.key),
     filter((key) => Directions.isDirectionKey(key)),
